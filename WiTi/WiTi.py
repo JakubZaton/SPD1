fd79d6319daa9e82fd240a345be4d8e5c5f05aa5
OlaStawarz <39085646+OlaStawarz@users.noreply.github.com>
2020-03-31 13:54:33 +0200

    Gotowy pierwszy etap

--- WiTi/WiTi.py
+++ WiTi/WiTi.py
@@ -1,3 +1,5 @@
+import itertools
+import sys
 
 class WiTi:
 
@@ -6,7 +8,7 @@ class WiTi:
         with open(sciezka) as f:
             n = f.readline()
             wyjscie = [[int(x) for x in line.split()] for line in f]
-            return wyjscie
+        return wyjscie
 
     @staticmethod
     def sortujD(data):
@@ -16,16 +18,40 @@ class WiTi:
 
     @staticmethod
     def funkcjeCelu(data):
-        Spi = 0
-        Cpi = []
-        T = 0
-        for x in range(0, len(data)):
-            Cpi[x] = Spi + data[x][0]
-            if Cpi[x] > data[x][2]:
-                T = T + ((Cpi[x] - data[x][2]) * data[x][1])
-            suma = suma + Cpi[x]
-            Spi = Cpi[x]
-        return T
+        S = []
+        C = []
+        T = []
+        F = []
+        S.append(0)
+        el = 0
+        for i in range(0, len(data) - 1):
+            el += data[i][0]
+            S.append(el)
+        el = 0
+        for i in range(0, len(data)):
+            el += data[i][0]
+            C.append(el)
+        for i in range(0, len(C)):
+            if C[i] > data[i][2]:
+                T.append(C[i] - data[i][2])
+            else:
+                T.append(0)
+        F_wynik = 0
+        for i in range(0, len(C)):
+            F.append(T[i] * data[i][1])
+            F_wynik += F[i]
+        return F_wynik
 
-data = WiTi.funkcjeCelu(WiTi.sortujD(WiTi.czytaj('data10.txt')))
-print(data)
+    @staticmethod
+    def licz_kombinacje(data):
+        comb = itertools.permutations(data, len(data))
+        F_wynik = sys.maxsize
+        for i in list(comb):
+            if WiTi.funkcjeCelu(i) < F_wynik:
+                F_wynik = WiTi.funkcjeCelu(i)
+        return F_wynik
+
+
+print(WiTi.funkcjeCelu(WiTi.sortujD(WiTi.czytaj('data10.txt'))))
+print(WiTi.licz_kombinacje(WiTi.czytaj('data10.txt')))
+
